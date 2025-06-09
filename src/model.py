e41e189846184e1c7c3e23c643767581b9150926
İnanç Çolak <colak.inanc12@gmail.com>
2025-06-09 16:49:11 +0300

    proje yapısı düzenlendi

--- src/model.py
+++
@@ -1,8 +0,0 @@
-import torch 
-from torchvision.models import efficientnet_b4, EfficientNet_B4_Weights
-
-def get_model(num_classes):
-    weights = EfficientNet_B4_Weights.DEFAULT
-    model = efficientnet_b4(weights=weights)
-    model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, num_classes)
-    return model
